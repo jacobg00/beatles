8f54bb88af8d878b73cad0e6efb88a2ed4d76806
William Campbell <whc7@pitt.edu>
2022-04-21 20:44:52 -0400

    adding sticky navbar js

---
+++ js/sticky.js
@@ -0,0 +1,20 @@
+function start () {
+    scroll();
+}
+window.onload = start;
+function scroll() {
+    window.onscroll = function () {
+        myFunction()
+    };
+    
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+    
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    }
+}
